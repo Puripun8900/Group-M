a27adae04c7c1144d5ca3764ad9e87ee2c00b77f
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-04-15 14:35:37 +0300

    Restructured server-side code into /server directory, created db/db.js for MongoDB connection, and improved code structure and logic

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/sensorRoutes.js');
+const connectMongoDB = require('./db/db')
+
+require('dotenv').config();
+//require('dotenv').config({path: path.resolve(__dirname, './.env')});
+
+const app = express();
+app.use(express.json());
+app.use('/sensors', routes);
+
+// serve static file
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Fallback route to serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+//Mongodb connection
+connectMongoDB();
+
+//server
+const port = process.env.PORT || 3000; // Use environment variable for port to host
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
